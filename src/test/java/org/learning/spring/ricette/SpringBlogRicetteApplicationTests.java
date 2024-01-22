3183150ce8170b6eeadac308bdf4cbb977263b95
luigilalo <luigilaloe1@gmail.com>
2024-01-22 09:24:15 +0100

    First Commit (Spring Initializr and Added DB)

---
+++ src/test/java/org/learning/spring/ricette/SpringBlogRicetteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.spring.ricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogRicetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
